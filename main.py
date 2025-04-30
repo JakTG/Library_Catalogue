9e886a100bb72f219b724d4e722be68ccbf0ce9c
Jakzenon23 <jak.snape@tonygee.com>
2025-04-30 12:38:07 +0100

    Update main.py
    Adding comments to show changes in:
    - Session states
    - Image upload
    - comments to do with next steps

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Packages that are being used in the app 
 import streamlit as st
 from PIL import Image, ImageEnhance, ImageFilter, ImageOps
 import pytesseract
@@ -5,7 +6,7 @@ import pandas as pd
 import io
 import numpy as np
 
-# Initialize session state to hold book data
+# Initialize session state to hold book data- add a refresh button to clear the session
 if 'book_data' not in st.session_state:
     st.session_state.book_data = []
 
@@ -14,7 +15,7 @@ st.image("https://www.workspace-interiors.co.uk/application/files/thumbnails/xs/
 st.title("Book OCR Extraction with Editable Catalogue")
 st.write("Automated app to extract information from images using OCR, allowing users to compile everything into a catalogued library and download it as an Excel file.")
 
-# How the app works expander
+# How the app works expander and how to use it
 with st.expander("How to use the app"):
     st.write(
         """
@@ -33,10 +34,12 @@ office = st.selectbox("Select Your Office", ["Manchester", "Esher", "Birmingham"
 uploaded_files = st.file_uploader("Choose all your images", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
 
 # Clear session state when new images are uploaded
+# Session state- maybe try and set it to so if you do leave the app or refresh the app it will clear the session
+# Needs to change from when you uploaded a book the session will change
 if uploaded_files:
     if 'last_uploaded_files' in st.session_state:
-        if set(file.name for file in uploaded_files) != set(st.session_state.last_uploaded_files):
-            st.session_state.book_data = []  # Reset catalog when new images are uploaded
+        if set(file.name for file in uploaded_files) != set(st.session_state.last_uploaded_files): # remove the last_ as this will be creating sessions on every last upload
+            st.session_state.book_data = []  # Reset catalog when new images are uploaded, this resets the catalogue- remove this line to disregard the resetting of the catalogue
     st.session_state.last_uploaded_files = [file.name for file in uploaded_files]
 
 def preprocess_image(image):
@@ -63,7 +66,7 @@ if uploaded_files:
         # Process image
         image_preprocessed = preprocess_image(image)
         
-        # Perform OCR with optimized settings
+        # Perform OCR with optimized settings- assuming a single column of text of variable sizes
         custom_config = "--psm 4 --oem 3"
         extracted_text = pytesseract.image_to_string(image_preprocessed, config=custom_config)
         
@@ -80,7 +83,7 @@ if uploaded_files:
         
         
         # Append to session state book data
-        new_entry = {"Image": file.name, "Title": title, "Edition": edition, "Author": author}
+        new_entry = {"Image": file, "Title": title, "Edition": edition, "Author": author}
         st.session_state.book_data.append(new_entry)
     
     st.success("All images processed and added to the catalog!")
